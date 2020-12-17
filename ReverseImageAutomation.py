1e33d0a4315a4873a63919d84285fb0d00c9ccb0
n <nicholas@dhaeyer.eu>
2020-12-17 17:36:21 +0100

    added new links

--- ReverseImageAutomation.py
+++ ReverseImageAutomation.py
@@ -12,16 +12,21 @@ def main():
         link = link.replace('/','%2F')
     except Exception as e:
         print("crash with reading the link. \nErrorcode: "+e)
+
     #open a webbrowser on the right page:
     webbrowser.open('https://www.bing.com/images/search?view=detailv2&iss=sbi&FORM=SBIHMP&sbisrc=UrlPaste&q=imgurl:'+link)
-    time.sleep(0.2)
+    time.sleep(0.4)
     webbrowser.open('https://yandex.com/images/search?url='+link+'&rpt=imageview')
-    time.sleep(0.2)
+    time.sleep(0.4)
     webbrowser.open('https://images.google.com/searchbyimage?image_url='+link)
-    time.sleep(0.2)
-    webbrowser.open('https://saucenao.com/search.php?url='+link)
-    time.sleep(0.2)
+    time.sleep(0.4)
     webbrowser.open('https://www.tineye.com/search?url='+link)
-    time.sleep(0.2)
+    time.sleep(0.4)
     webbrowser.open('https://iqdb.org/?url='+link)
+    time.sleep(0.4)
+    webbrowser.open('https://saucenao.com/search.php?url='+link)
+    
+    #needed if you don't have firefox running already
+    print('Press enter to close...')
+    input()
 main()
